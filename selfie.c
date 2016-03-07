37bb44de7a34546b138a1bfb834dabb699423cd1
phuber <huberpa@stud.sbg.ac.at>
2016-03-07 16:36:03 +0100

    Assignment 0 done

--- selfie.c
+++ selfie.c
@@ -6428,4 +6428,5 @@ int main(int argc, int *argv) {
         print((int*) ": usage: selfie { -c source | -o binary | -s assembly | -l binary } [ -m size ... | -d size ... | -y size ... ] ");
         println();
     }
+	return 0;
 }
